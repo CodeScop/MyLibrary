3cc6f62a12582f88e3ac9b6b2003ac5256f92320
CodeScop <StefBWard@gmail.com>
2016-08-15 20:28:07 -0400

    Novels view added

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -1,5 +1,17 @@
 class NovelsController < ApplicationController
+before_action :novel_length, only: [:index]
   def index
   	@books = Book.all
+  end
+
+  def novel_length
+  	@books = Book.all
+    @novel_length_books = []
+
+    @books.each do |book|
+         if book.number_of_pages.to_i >= 240
+             @novel_length_books.push(book)
+         end
+     end
   end
 end
